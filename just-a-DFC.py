4176cc671be4aa5bfafd65916d7a2bb6a74ed106
Kalam :p <63474858+YourKalamity@users.noreply.github.com>
2020-08-24 12:51:29 +0100

    Cleaned up code

--- just-a-DFC.py
+++ just-a-DFC.py
@@ -11,6 +11,7 @@ import tkinter.font
 from tkinter import filedialog
 from tkinter import messagebox
 import threading
+import tkinter.ttk
 
 def chooseDir(source,entryBox):
     source.sourceFolder =  filedialog.askdirectory(parent=source, initialdir= "/", title='Please select the directory of your SD card')
@@ -38,25 +39,6 @@ def getFreeSpace(dirname):
     else:
         st = os.statvfs(dirname)
         return st.f_bavail * st.f_frsize / 1024 / 1024 /1024
-def getDriveName():
-    while True:
-        try:
-            driveName = str(input("Enter the drive name of your SD card :   "))
-        except TypeError:
-            print("That's not a valid input")
-            continue
-        try:
-            directory = '/Volumes/' + driveName + "/dummy1"
-            with open(directory, 'ab') as file:
-                file.close()
-        except FileNotFoundError:
-            print("You do not have write access or the drive could not be found")
-            continue
-        except PermissionError:
-            print("You do not have write access")
-            continue
-        else:
-            return driveName
 
 def donothing():
     return
@@ -64,74 +46,11 @@ def donothing():
 def closeButtonPress(source):
     source.destroy()
 
-def getDriveDirectory():
-    while True:
-        try:
-            print("Enter the FULL directory of your SD card")
-            print("For example '/dev/sda1' without the quotation marks")
-            driveDirectory = str(input("Full directory of your SD card : "))
-            if driveDirectory[-1] == '/' :
-                driveDirectory = driveDirectory[:--1]
-            
-        except TypeError:
-            print("That's not a valid input")
-            continue
-        try:
-            directory = driveDirectory + "/dummy1"
-            with open(directory, 'ab') as file:
-                file.close()
-        except FileNotFoundError:
-            print("You do not have write access or the drive could not be found")
-            continue
-        except PermissionError:
-            print("You do not have write access")
-            continue
-        else:
-            return driveDirectory
-
-def getDriveLetter():
-    while True:
-        try:
-            driveLetter = str(input("Enter the drive letter of your SD card :   "))
-            driveLetter = driveLetter.upper()
-        except TypeError:
-            print("That's not a valid input")
-            continue
-        if len(driveLetter) != 1:
-            print("That's not a valid input")
-            continue
-        if driveLetter not in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or driveLetter == '/' :
-            print("That's not a valid input")
-            continue
-        try:
-            directory = driveLetter + ":/dummy1"
-            with open(directory, 'ab') as file:
-                file.close()
-        except FileNotFoundError:
-            print("You do not have write access or the drive could not be found")
-            continue
-        except PermissionError:
-            print("You do not have write access")
-            continue
-        else:
-            return driveLetter
-            break
-def dummyFilesNeeded(freeSpace):
-    x =  freeSpace % 4
-    if x <= 2:
-        print("Dummy files aren't necessary!")
-        print("The application will now close")
-        sys.exit()
-    else:
-        z = x - 2
-        print(z,"GB of dummy files are needed")
-        return z
 
 def createDummyFiles(amount,dummyFileLocation):
     outputbox("Creating...")
     outputbox("Please wait...")
     with open(dummyFileLocation, 'ab') as dummyFile:
-        numChars = amount * 1024 * 1024 * 1024  + 10240000
         x = 0
         toWrite = '1'.encode()
         while x < numChars:
@@ -140,7 +59,7 @@ def createDummyFiles(amount,dummyFileLocation):
 
     outputbox("DF Created")
     outputbox("Close App")
-    sys.exit()
+    return
 
 def start(location,source):
     window.protocol("WM_DELETE_WINDOW",lambda:donothing)
@@ -191,6 +110,7 @@ def main():
     global window
     window = tkinter.Tk()
     window.minsize(100,100)
+    window.resizable(0,0)
     titleFont = tkinter.font.Font(size=20)
     title = tkinter.Label(window, text="just-a-DFC", font=titleFont)
     title.grid(row=0,column=0,sticky="w")
@@ -203,41 +123,12 @@ def main():
     outputBox.grid(column=0,row=4,sticky="w")
     global startButton
     startButton = tkinter.Button(window,text="Start", command =lambda:threadFunction(SDEntry.get(),window,), width=10, font=("Segoe UI", 11))
-    startButton.grid(column=0,row=5)
+    startButton.grid(column=0,row=6)
+    
+    
 
 
     window.mainloop()
-    
-    print("Just Kalam's just a dummy file creator (for hiyaCFW)")
-    print(" ")
-    print("This program will calculate if a dummy file is needed ")
-    print("to insure hiyaCFW does not boot into the 'An error has")
-    print("occured' screen")
-    print("")
-    if platform.system() == 'Windows':
-        print('Platform : "Windows" ')
-        driveLetter = getDriveLetter()
-        directory = driveLetter + ":/"
-        freeSpace = getFreeSpace(directory)
-    elif platform.system() == 'Darwin':
-        print('Platform : "MacOS X" ')
-        driveName = getDriveName()
-        directory = '/Volumes/' + driveName + "/"
-        freeSpace = getFreeSpace(directory)
-    elif platform.system() == 'Linux':
-        print('Platform : "Linux"')
-        driveDirectory = getDriveDirectory()
-        freeSpace = getFreeSpace(driveDirectory)
-    else:
-        sys.exit()
-        
-    print("Free space remaining (in GB) = ", freeSpace)
-
-    sizeOfDummyFiles = dummyFilesNeeded(freeSpace)
-
-    dummyFileLocation = directory + "dummy1"
-
-    createDummyFiles(sizeOfDummyFiles,dummyFileLocation)
 
 main()
 
